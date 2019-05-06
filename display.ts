033992e5f69594e5675e7412165f2d505fbf830d
SteveCollie <steve.collie@finbourne.com>
2019-05-06 13:54:06 +0100

    Updated display.ts

--- display.ts
+++ display.ts
@@ -1,8 +1,5 @@
 /**
- * Defines a custom function showSignal which plots the signal
- * strength from the radio receiver using all the pixels on the display.
- * The return value indicates if the signal is above a certain threshold, allowing
- * the caller to run custom 'found' logic.
+ * Defines useful display functions for Micro:bit
  */
 
 //% weight=100 color=#0fbc11 icon="\uf1ec" 
@@ -10,31 +7,22 @@ namespace display {
     /**
      * Display a radio signal level on the LED display
      * @param signal the radio signal level
+     * @param lower the lower range of signal
+     * @param upper the upper range of signal
      */
     //% block
-    export function showSignal(signal: number) {
-        // Signal is -128 to -42, range of 86
-        // We have 5*5 leds, 25 in total
-
+    export function showSignal(signal: number, lower: number, upper: number) {
         let leds: number
-        // First convert to 0-86
-        leds = signal + 128
 
-        // Then divide by three so range is 0-28.33
-        leds = Math.floor(leds / 3)
+        // First rebase to 0
+        leds = signal - lower
+
+        // Then convert to 0-25
+        leds = leds * 25 / (upper - lower)
 
         // Switch on the leds starting at the bottom left
         // This covers the range from 0-25
         show(leds)
-
-        if (leds > 24) {
-            // To handle 25 to 28.33, increase the brightness
-            // from 128 to 255 in steps of 32
-            led.setBrightness(128 + (leds - 24) * 32)
-        }
-        else {
-            led.setBrightness(128)
-        }
     }
 
     // Ensure that we always clear the screen on the first execution
