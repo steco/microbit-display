0c7fdbd00e137a8210c8268397ca5e2a15c56604
steco <steve@thecollies.co.uk>
2019-12-01 13:23:34 +0000

    Fixes tests

--- tests.ts
+++ tests.ts
@@ -1,22 +1,22 @@
 // Draw an up-arrow
 display.drawArrow(0)
-basic.pause(1000)
+basic.pause(500)
 
 // Draw a down arrow
 display.drawArrow(8)
-basic.pause(1000)
+basic.pause(500)
 
 // Slowly increase and decrease the number being displayed
 for (let i: number = 0; i < 51; ++i) {
     let j: number = i < 25 ? i : 50 - i
     display.show(j)
-    basic.pause(100)
+    basic.pause(10)
 }
 
 // Show a gradually increasing signal strength
 for (let signalStrength: number = -128; signalStrength < -46; ++signalStrength) {
-    display.showSignal(signalStrength)
-    basic.pause(100)
+    display.showSignal(signalStrength, -128, -46)
+    basic.pause(10)
 }
 
 // Done!
